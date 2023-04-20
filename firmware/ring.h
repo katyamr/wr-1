0b87fb3e382726f1366a815e4dd2775af8c31ecc
Max Romanov <max.romanov@gmail.com>
2023-04-20 09:23:37 +0300

    ring: Adjust capacity and template parameter value
    It is more convenient to have predictable number of elements stored
    in the ring without implicit +/- 1 in mind.

--- firmware/ring.h
+++ firmware/ring.h
@@ -18,15 +18,16 @@ template<> struct uint_<2> { using type = uint16_t; };
 template<uint16_t MAX, typename T>
 struct ring {
     using value_t = T;
-    using index_t = typename uint_< size_bytes<MAX - 1>::value >::type;
+    using index_t = typename uint_< size_bytes<MAX>::value >::type;
 
     enum {
-        capacity = MAX
+        capacity = MAX,
+        data_size = MAX + 1,
     };
 
     bool empty() const { return begin == end; }
-    bool full() const { return begin == (end + 1) % MAX; }
-    index_t size() const { return (end + MAX - begin) % MAX; }
+    bool full() const { return begin == (end + 1) % data_size; }
+    index_t size() const { return (end + data_size - begin) % data_size; }
 
     void reset() {
         begin = 0;
@@ -39,21 +40,21 @@ struct ring {
 
     void push_back(value_t v) {
         data[end] = v;
-        end = (end + 1) % MAX;
+        end = (end + 1) % data_size;
     }
 
     void back(value_t v) {
-        data[(MAX + end - 1) % MAX] = v;
+        data[(data_size + end - 1) % data_size] = v;
     }
 
     value_t pop_front() {
         value_t res = data[begin];
-        begin = (begin + 1) % MAX;
+        begin = (begin + 1) % data_size;
 
         return res;
     }
 
     index_t begin = 0;
     index_t end = 0;
-    value_t data[MAX];
+    value_t data[data_size];
 };
